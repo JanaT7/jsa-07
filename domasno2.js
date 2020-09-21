eb288731ccaf6ddad9bae15c4d4498e5bb2517e6
TemovskaJana <58231275+TemovskaJana@users.noreply.github.com>
2020-09-21 22:00:32 +0200

    Add files via upload

---
+++ domasno2.js
@@ -0,0 +1,48 @@
+function kmToml (kilometar) {
+   let miles =  kilometar * 0.62137
+   return miles
+};
+
+kmToml();
+
+console.log(kmToml(10));
+
+
+function kgTolb (kilogram) {
+    let pounds =  kilogram * 2.2046
+    return pounds
+ };
+ 
+ kgTolb();
+ 
+ console.log(kgTolb(56));
+
+
+ function liToga (litra) {
+    let gallon =  litra * 3.78541
+    return gallon
+ };
+ 
+ liToga();
+ 
+ console.log(liToga(40));
+
+
+ function cmTom (centimetar) {
+    let metar =  centimetar / 100
+    return metar
+ };
+ 
+ cmTom();
+ 
+ console.log(cmTom(578));
+
+
+ function feetTokm (feets) {
+    let kilometar =  feets / 3280.8
+    return kilometar
+ };
+ 
+ feetTokm();
+ 
+ console.log(feetTokm(8635));
